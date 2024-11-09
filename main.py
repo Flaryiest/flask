bff8ca00eda6370ee9632bb334ae440635799e57
Flaryiest <ericmzuo1@gmail.com>
2024-11-08 20:30:41 -0700

    temp

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
-
+from flask import Flask, jsonify
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return render_template('index.html')
-
-if __name__ == '__main__':
-  app.run(port=5000)
+app.route("/hello_world", methods=["GET", "POST"])
+def hello_world():
+    return jsonify({"message": "hello world"})
+if __name__ == "__main__":
+	app.run(host="127.0.0.1", port=3333)
